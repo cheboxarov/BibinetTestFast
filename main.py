aacaaef88b8181c1fd9600986e20d5740bdd8d12
wlovem <lalakasuper2@gmail.com>
2024-07-13 17:54:02 +0700

    Added connection to the database via "databases".

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 from fastapi import FastAPI
+from databases import Database
+import os
+
+DATABASE_URL = (f"postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@"
+                f"{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}")
+
+database = Database(DATABASE_URL)
 
 app = FastAPI()
 
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()

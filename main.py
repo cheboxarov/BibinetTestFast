5279d449ea16b7b92743274428e71c3323b8ce3a
wlovem <lalakasuper2@gmail.com>
2024-07-13 18:16:33 +0700

    Fixed DATABASE_URL

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from databases import Database
 import os
 import json
 
-DATABASE_URL = (f"postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@"
-                f"{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}")
+DATABASE_URL = f"postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}"
 
 database = Database(DATABASE_URL)
 
